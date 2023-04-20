8ba68b36510c4a4cd9f9b13ff1ed9e137db610fa
Francisco Sanchez <c0d3cisco@yahoo.com>
2023-04-20 09:34:01 -0500

    lab 08

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // **** REQUIRE *** (lime import but for the backend)
 
 const express = require('express');
+const axios = require('axios');
 require('dotenv').config();
 const cors = require('cors');
 let weatherData = require('./data/weather.json');
@@ -25,26 +26,27 @@ app.listen(PORT, () => {
 // *** 1st arg - endpoint url
 // *** 2nd arg - callback which will execute when that endpoint is hit
 //           *** 2 parameters: request req, response res
-app.get('/',(req, res) => {
+app.get('/',(req, res, next) => {
     res.status(200).send('Welcome to my server!'); // this is what you are sending. You're sending the status of 200
 });
 
-app.get('/hello', (req, res) => {
-    let firstName = req.query.firstName;
-    let lastName = req.query.lastName;
-    console.log(req.query);
-    res.status(200).send(`Hello, ${firstName} ${lastName} welcome to my server!`)
-})
-
-
 // GOAL OF THIS IS TO RETURN CITY SEARCHED
 
-app.get('/weather', (req, res, next) => {
+app.get('/weather', async (req, res, next) => {
     try{
-        let queriedSpecies = req.query.cityName;
-
-        let cityDataArr = weatherData.find(city => city.city_name.toLocaleLowerCase() === queriedSpecies.toLocaleLowerCase()); //this line searches the big json and returns a smaller but bulky array
-        let dataToSend = cityDataArr.data.map(cityDataObj =>  new WeatherInfo(cityDataObj)); // this line is to trim down the bulkiness
+        let lat = req.query.lat;
+        let lon = req.query.lon;
+        console.log(lat,lon);
+        let urlWeather = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
+        //TODO: store axios data
+        let weatherData = await axios.get(urlWeather);
+        // console.log(weatherData.data);
+        //TODO: take that result from axious and groom down below after this function
+        let dataToSend = weatherData.data.data.map(cityDataObj =>  new WeatherInfo(cityDataObj));
+        // dataToSend.push(weatherData.data.city_name);
+        console.log(dataToSend)
+        //TODO: groom data and send it to the response
+        // if its too big, it will say it is circular in nature
         res.status(200).send(dataToSend);
     }
     catch(error){
@@ -53,11 +55,43 @@ app.get('/weather', (req, res, next) => {
 });
 
 
+app.get('/movie', async (req, res, next) => {
+    try{
+        //TODD: accept or define my queries -> /photos?searchQuery=VALUE.... searchQUERY = city in the example below.
+        let targetCity = req.query.cityName;
+        //TODO: build out my url for axios
+        let urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query=${targetCity}`;
+        //TODO: store axios data
+        let movieData = await axios.get(urlMovie);
+        //TODO: take that result from axios and groom down below after this function
+        let englishMovies  = movieData.data.results.filter(movie => movie.original_language === 'en' && movie.backdrop_path);
+        console.log(englishMovies);
+        let groomMovieData = englishMovies.map(movie => new MovieInfo(movie));
+        //TODO: groom data and send it to the response
+        // res.status(200).send(movieData.data);
+        res.status(200).send(groomMovieData);
+    }
+    catch(error){
+       next(error)
+    }   
+});
+
+//TODO: define my photo class and info I want to render to the 
+
 // *** CLASS TO GROOM BUILKY DATA ***
 class WeatherInfo{
     constructor(cityObj){
         this.weatherDescription = cityObj.weather.description,
         this.weatherDate = cityObj.valid_date
+    };
+}
+
+class MovieInfo{
+    constructor(movieObj){
+        this.movieTitle = movieObj.original_title,
+        this.movieReleaseDate = movieObj.release_date,
+        this.movieDescription = movieObj.overview,
+        this.movieImage = 'https://image.tmdb.org/t/p/w500' + movieObj.poster_path
     };
 }
 
