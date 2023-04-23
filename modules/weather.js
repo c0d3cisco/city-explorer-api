acb3f5f7c67b12c40d5c8e43adc45263f329086a
Francisco Sanchez <c0d3cisco@yahoo.com>
2023-04-23 18:21:55 -0500

    configuring cache

--- modules/weather.js
+++ modules/weather.js
@@ -2,11 +2,21 @@
 
 const axios = require('axios');
 
+let cache = {};
+console.log(cache);
+
 async function getWeather(req,res,next){
     try{
         let lat = req.query.lat;
         let lon = req.query.lon;
-        console.log(lat,lon);
+        let keyCoor = `${lat, lon}-weather`
+        // console.log(lat,lon);
+        if(cache[keyCoor] && (Date.now() - cache[keyCoor].timeStamp) < 43200000){
+            console.log('cache was hit!', cache[keyCoor]);
+            res.status(200).send(cache[keyCoor].data);
+        } else {
+
+            console.log('cache was missed');
         let urlWeather = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
         //TODO: store axios data
         let weatherData = await axios.get(urlWeather);
@@ -14,10 +24,16 @@ async function getWeather(req,res,next){
         //TODO: take that result from axious and groom down below after this function
         let dataToSend = weatherData.data.data.map(cityDataObj =>  new WeatherInfo(cityDataObj));
         // dataToSend.push(weatherData.data.city_name);
-        console.log(dataToSend)
+        // console.log(dataToSend)
         //TODO: groom data and send it to the response
         // if its too big, it will say it is circular in nature
+        cache[keyCoor] = {
+            data: dataToSend,
+            timeStamp: Date.now()
+        }
+        console.log('cache was saved!', cache[keyCoor]);
         res.status(200).send(dataToSend);
+        }
     }
     catch(error){
        next(error)
--- server.js
+++ server.js
@@ -38,28 +38,6 @@ app.get('/weather', getWeather);
 const getMovie = require('./modules/movie')
 app.get('/movie', getMovie);
 
-// async function getMovie(req, res, next)  {
-//     try{
-//         //TODD: accept or define my queries -> /photos?searchQuery=VALUE.... searchQUERY = city in the example below.
-//         let targetCity = req.query.cityName;
-//         //TODO: build out my url for axios
-//         let urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query=${targetCity}`;
-//         //TODO: store axios data
-//         let movieData = await axios.get(urlMovie);
-//         //TODO: take that result from axios and groom down below after this function
-//         let englishMovies  = movieData.data.results.filter(movie => movie.original_language === 'en' && movie.backdrop_path);
-//         console.log(englishMovies);
-//         let groomMovieData = englishMovies.map(movie => new MovieInfo(movie));
-//         //TODO: groom data and send it to the response
-//         // res.status(200).send(movieData.data);
-//         res.status(200).send(groomMovieData);
-//     }
-//     catch(error){
-//        next(error)
-//     }   
-// };
-
-
 app.get('/test', async (req, res, next) => {
     try{
         res.status(200).send('this server is working... the issue is your API key');
@@ -68,10 +46,6 @@ app.get('/test', async (req, res, next) => {
        next(error)
     }   
 });
-
-//TODO: define my photo class and info I want to render to the 
-
-// *** CLASS TO GROOM BUILKY DATA ***
 
 
 class MovieInfo{
